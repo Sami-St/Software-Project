bd8d8a8715fca0db27a3cc4d11e13ba8730b3471
Basil902 <103893854+Basil902@users.noreply.github.com>
2024-12-26 13:17:17 +0100

    Update models.py file

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
 
 class User(BaseModel):
 
+    userID: Optional[int] = None
     name: str = Field(default=None)
     email: EmailStr = Field(default=None)
     password: str = Field(default=None)
-    userType : str = Field(default=None)
+    rolle : str = Field(default=None)
     fächer: list[str] = Field(default=None)
