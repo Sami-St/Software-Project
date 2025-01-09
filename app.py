477ccdff70e465fd8ddac51b8e55e58a7b892b5d
Basil902 <103893854+Basil902@users.noreply.github.com>
2025-01-09 15:50:55 +0100

    Add new config.py file; modify app.py

--- app.py
+++ app.py
@@ -1,41 +1,21 @@
-from flask import Flask, render_template, redirect, url_for, jsonify
-from flask_jwt_extended import JWTManager, get_jwt_identity, jwt_required
-from routes import authentication
-from utils.token_utils import secret_key
+from flask import render_template, redirect, url_for, session
+from flask_login import login_required
+from config import create_app
 
+app = create_app()
 
-app = Flask(__name__)
-jwt = JWTManager(app)
-app.config['SECRET_KEY'] = secret_key
-app.config["JWT_SECRET_KEY"] = secret_key
-
-app.register_blueprint(authentication)
-
-@jwt.unauthorized_loader
-def unauthorized_error(callback):
-
-    return render_template("unauthorized.html")
-
-@app.route('/')
+@app.route("/", methods=["GET"])
 def index():
 
     return redirect(url_for("authentication.register"))
 
-@app.route('/home', methods=['GET'])
-@jwt_required()
-def home():
 
-    print("inside home")
+@app.route('/dashboard', methods=['GET'])
+@login_required
+def dashboard():
 
-    user = get_jwt_identity()
-
-    if user:
-
-        return render_template("home.html") 
-    
-    else:
-
-        return jsonify({"message": "couldnt identify user"}), 404
+    return render_template("dashboard.html", role=session["role"])
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ config.py
@@ -0,0 +1,61 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+session = Session()
+login_manager = LoginManager()
+secret_key = "3b1c60e954f646299c20e9c6b09a0b7d"
+
+
+from models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+
+    return db.session.get(User, int(user_id))
+
+def create_app():
+
+    app = Flask(__name__)
+
+    db_file_path = '../software_projekt/database/datenbank.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.abspath(db_file_path)}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = secret_key
+    app.config['SESSION_TYPE'] = 'sqlalchemy'
+    app.config['SESSION_SQLALCHEMY'] = db
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    session.init_app(app)
+    login_manager.init_app(app)
+    
+
+    from routes import authentication, user_interactions
+    app.register_blueprint(authentication)
+    app.register_blueprint(user_interactions)
+
+    login_manager.login_view = "authentication.login"
+
+    # create tables defined in models.py in case they don't exist
+    with app.app_context():
+        db.create_all()
+
+    # delete all rows in the specified table
+    # from models import Anwesenheit
+    # with app.app_context():
+    #     db.session.query(Anwesenheit).delete()
+    #     db.session.commit()
+    
+    # delete specified table
+    # from sqlalchemy import text
+    # with app.app_context():
+    #     with db.engine.connect() as connection:
+    #         connection.execute(text('DROP TABLE IF EXISTS table'))
+
+    return app
